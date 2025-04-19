5a9fb824b66a7b40c1227b83cc659194014424d3
nishi-uthappa <nishithauthappa97@gmail.com>
2025-04-19 11:54:56 +0530

    first commit

---
+++ sql load/create_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE retail_sales (
+    transactions_id INT PRIMARY KEY,
+    sale_date DATE,
+    sale_time TIME,
+    customer_id INT,
+    gender VARCHAR(15),
+    age INT,
+    category VARCHAR(15),
+    quantity INT,
+    price_per_unit FLOAT,
+    cogs FLOAT,
+    total_sale FLOAT
+);
